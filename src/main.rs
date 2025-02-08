cd02b1bc165123b8e461c0b8f0f408b1a20936d4
wgptross <gwptross@proton.me>
2025-02-08 15:13:25 -0700

    config file rewrite

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,36 @@ fn read_json_file(path: &str) -> Result<Value, std::io::Error> {
     let user = json_data
         .get("database")
         .ok_or_else(|| Error::new(ErrorKind::InvalidData, "error invalid"))?;
-
+    //return json data
     Ok(user.clone())
 }
 
 fn main() {
-    let x = read_json_file("./config/config.json");
-    println!("{:?}", x);
+    let json_file_path = "./config/config.json";
+
+    match read_json_file(json_file_path) {
+        Ok(user) => {
+            match user.get("database") {
+                Some(database) => {
+                    let user = database.get("location").unwrap_or(&Value::Null)as_str().unwrap_or("unknown");
+
+
+                    println!("location: {}", user);
+
+                },
+                None => {
+                    println!("not found in table");
+                }
+            }
+        },
+        Err(e) => {
+            println!("error reading json")
+        }
+    }
+
+
+    // let x = read_json_file("./config/config.json");
+    // println!("{:?}", x);
+    //
+    println!("{}", user)
 }
