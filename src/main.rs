8b84f3f82397f42052809e3c5f3bf4d21d348979
wgptross <gwptross@proton.me>
2025-02-09 13:33:32 -0700

    rewrite of config reader function(s)

--- src/main.rs
+++ src/main.rs
@@ -3,43 +3,94 @@ use serde_json::Value;
 use std::fs::File;
 use std::io::{BufReader, Error, ErrorKind};
 
-fn read_json_file(path: &str) -> Result<Value, std::io::Error> {
-    let file = File::open(path)?;
+fn ex() -> Result<Value, Error> {
+    let file = File::open("./config/config.json")?;
     let reader = BufReader::new(file);
     let json_data: Value = serde_json::from_reader(reader)?;
-    let user = json_data
+    let database = json_data
         .get("database")
         .ok_or_else(|| Error::new(ErrorKind::InvalidData, "error invalid"))?;
-    //return json data
-    Ok(user.clone())
+
+    Ok(database.clone())
+}
+
+// fn read_json_file(path: &str) -> Result<Value, std::io::Error> {
+//     let file = File::open(path)?;
+//     let reader = BufReader::new(file);
+//     let json_data: Value = serde_json::from_reader(reader)?;
+//     let user = json_data
+//         .get("database")
+//         .ok_or_else(|| Error::new(ErrorKind::InvalidData, "error invalid"))?;
+//     //return json data
+//     Ok(user.clone())
+// }
+
+//this function takes databse Value and parses specific programed data to a string to be called in other functions
+fn parse_config(database: &Value) -> Result<String, Error> {
+    if let Some(host) = database.get("location").and_then(Value::as_str) {
+        // return string value(s)
+        Ok(host.to_string())
+    } else {
+        //handle error
+        Err(Error::new(ErrorKind::NotFound, "not found"))
+    }
 }
 
 fn main() {
-    let json_file_path = "./config/config.json";
-
-    match read_json_file(json_file_path) {
-        Ok(user) => {
-            match user.get("database") {
-                Some(database) => {
-                    let user = database.get("location").unwrap_or(&Value::Null)as_str().unwrap_or("unknown");
-
-
-                    println!("location: {}", user);
-
-                },
-                None => {
-                    println!("not found in table");
-                }
+    match ex() {
+        // match main ex witch reads orignal database values
+        // get these values and then match to the parse_config func
+        Ok(database) => match parse_config(&database) {
+            Ok(host_str) => {
+                //string data now callable throughout program
+                println!("calling here {}", host_str)
+            }
+            Err(err) => {
+                println!("error handle {}", err)
             }
         },
-        Err(e) => {
-            println!("error reading json")
+        //handle error reading file
+        Err(err) => {
+            println!("error reading file {}", err)
         }
     }
 
+    // match ex() {
+    //     Ok(database) => {
+    //         println!("database object found");
+    //         parse_config(&database);
+    //     }
+    //     Err(err) => {
+    //         println!("error reading or parsing config {}", err);
+    //     }
+    // }
 
-    // let x = read_json_file("./config/config.json");
-    // println!("{:?}", x);
-    //
-    println!("{}", user)
+    // let ex = ex();
+
+    // println!("{:?}", ex);
+
+    // let json_file_path = "./config/config.json";
+
+    // match read_json_file(json_file_path) {
+    //     Ok(user) => match user.get("database") {
+    //         Some(database) => {
+    //             let location = database
+    //                 .get("location")
+    //                 .unwrap_or(&Value::Null)
+    //                 .as_str()
+    //                 .unwrap_or("unknown");
+
+    //             println!("location: {}", location);
+    //         }
+    //         None => {
+    //             println!("not found in table");
+    //         }
+    //     },
+    //     Err(e) => {
+    //         println!("error reading json {}", e)
+    //     }
+    // }
+    // // let x = read_json_file("./config/config.json");
+    // // println!("{:?}", x);
+    // //
 }
