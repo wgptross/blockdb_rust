690ebe0266ec0edc1903096ab70f0a971517fde3
wgptross <gwptross@proton.me>
2025-02-15 11:24:02 -0700

    writing to file db

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 //use serde::{Deserialize, Serialize};
 use serde_json::Value;
-use std::fmt::Display;
 use std::fs::{File, OpenOptions};
 use std::io::{BufReader, Error, ErrorKind};
 use std::path::Path;
@@ -31,7 +30,28 @@ struct BlockDB {
     databasefile: String,
 }
 
-fn write_to_db(db: &String, file: &str) {
+fn db_index_update(db: &String, index_id: u32) {
+    let local_db = String::from(db);
+    let mut file = match OpenOptions::new().create(true).append(true).open(&local_db) {
+        Err(why) => panic!("could no open index {}", why),
+        Ok(file) => file,
+    };
+
+    let id = index_id.to_string();
+    let index_id_ref = &id;
+
+    match file.write(index_id_ref.as_bytes()) {
+        Err(why) => panic!("error writing {}", why),
+        Ok(_) => println!("updated index"),
+    };
+}
+
+// enum InsertValue {
+//     String(String),
+//     Int(u32),
+// }
+
+fn write_to_db(db: &str, file: &str, value: &str, id: u32) {
     let mut local_db = String::from(db);
     local_db.push_str("/");
     local_db.push_str(file);
@@ -44,7 +64,14 @@ fn write_to_db(db: &String, file: &str) {
         Ok(file) => file,
     };
 
-    match file.write("testing\n".as_bytes()) {
+    let mut value_format = String::from(value);
+    let id_string = id.to_string();
+    let s_slice: &str = &id_string[..];
+
+    value_format.push_str(s_slice);
+    value_format.push_str("\n");
+
+    match file.write(value_format.as_bytes()) {
         Err(why) => panic!("could not write {}, {}", dis, why),
         Ok(_) => println!("success {}", dis),
     };
@@ -71,11 +98,12 @@ impl BlockDB {
         &self
     }
 
-    fn put(&self, value: String, id: u32) {
+    fn put(&self, database_location: &str, database_table: &str, value: &str, id: u32) {
         println!(
             "{} calling put other: {} at {}",
             &self.databasefile, value, id
         );
+        write_to_db(database_location, database_table, "testing", id);
     }
 
     fn get(&self, id: u32) {
@@ -123,10 +151,10 @@ fn main() {
             let database_call = BlockDB { databasefile: x };
             let database_main = BlockDB::open(&database_call);
             println!("{}", database_main.databasefile);
-            database_call.put(String::from("this"), 12);
+            database_call.put(&database_main.databasefile, "testing", "value", 13);
             database_call.get(12);
 
-            write_to_db(&database_main.databasefile, "testing");
+            // write_to_db(&database_main.databasefile, "testing", "value", 13);
         }
         Err(err) => {
             println!("error handle file open {}", err);
